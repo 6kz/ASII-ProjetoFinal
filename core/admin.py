56b8ce287900802052307cf24ee80c47a627683d
Tomás Matos <66218733+6kz@users.noreply.github.com>
2026-05-20 19:54:42 +0100

    Desenvolvimento da pagina inicial e sistema de publicacoes

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Post, Comentario
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("autor", "conteudo_curto", "criado_em")
+
+    def conteudo_curto(self, obj):
+        return obj.conteudo[:50]
+
+    conteudo_curto.short_description = "Conteúdo"
+
+
+# Regista o modelo Comentario no painel de administração
+@admin.register(Comentario)
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display = ("autor", "post_id", "conteudo_curto", "criado_em")
+
+    def conteudo_curto(self, obj):
+        return obj.conteudo[:50]
+
+    conteudo_curto.short_description = "Conteúdo"
--- core/views.py
+++ core/views.py
@@ -1,16 +1,19 @@
-from django.shortcuts import render, get_object_or_400
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from .models import Post
 
+
 def index(request):
-    posts = Post.objects.all().order_by('-criado_em')
-    return render(request, 'index.html', {'posts': posts})
+    posts = Post.objects.all().order_by("-criado_em")
+    return render(request, "index.html", {"posts": posts})
+
 
 def post_detail(request, post_id):
-    post = get_object_or_400(Post, id=post_id)
-    return render(request, 'post_detail.html', {'post': post})
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, "post_detail.html", {"post": post})
+
 
 def user_profile(request, username):
-    user = get_object_or_400(User, username=username)
-    posts = user.posts.all().order_by('-criado_em')
-    return render(request, 'profile.html', {'profile_user': user, 'posts': posts})
+    user = get_object_or_404(User, username=username)
+    posts = user.posts.all().order_by("-criado_em")
+    return render(request, "profile.html", {"profile_user": user, "posts": posts})
